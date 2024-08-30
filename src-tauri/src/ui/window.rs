c72fe2d9717d7f1d8b5cad7865395caec3007743
AmberWagner <wzhzzmzzy@gmail.com>
2024-08-30 10:10:39 +0800

    fix(tray): sync tray menu text & state since window's visiblity change

--- src-tauri/src/ui/window.rs
+++ src-tauri/src/ui/window.rs
@@ -12,3 +12,33 @@ pub fn create_main_window(app: AppHandle) -> Result<Window> {
         .title("")
         .build()
 }
+
+
+pub fn create_settings_window(app: AppHandle) -> Result<Window> {
+    tauri::WindowBuilder::new(
+        &app,
+        "settings",
+        tauri::WindowUrl::App("settings".into()),
+    )
+        .title("设置")
+        .inner_size(f64::from(229), f64::from(115))
+        .build()
+}
+
+pub fn toggle_main_window(app: &AppHandle) -> bool {
+    if let Some(window) = app.get_window("main") {
+        if let Ok(window_visible) = window.is_visible() {
+            if window_visible {
+                window.hide().unwrap();
+                false
+            } else {
+                window.show().unwrap();
+                window.set_focus().unwrap();
+                true
+            }
+        } else { false }
+    } else {
+        create_main_window(app.clone()).unwrap();
+        true
+    }
+}
