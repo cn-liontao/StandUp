2fe5b3918aa3c39eb9357395417eeddff398ceae
AmberWagner <wzhzzmzzy@gmail.com>
2024-09-08 14:26:48 +0800

    fix(hint): show tooltip upon mouseover switcher

--- src/components/header/pagination.svelte
+++ src/components/header/pagination.svelte
@@ -2,9 +2,22 @@
 	import { calendarState } from '$store/calendar'
 	import { standing } from '$store/day-records';
 	import { invoke } from '@tauri-apps/api/tauri';
+	import { useTooltip } from '$components/tooltip/index.svelte';
+	import { formatDuration, intervalToDuration, startOfToday } from 'date-fns';
 
+	const tooltipContent = useTooltip()
 	const stand_or_sit = () => {
 		invoke("stand_or_sit")
+		tooltipContent.set('')
+	}
+
+	const showStandingStatus = () => {
+		if (!$standing) {
+			tooltipContent.set('点击可以开始站立')
+			return
+		}
+		const duration = intervalToDuration({ start: $standing, end: Date.now() })
+		tooltipContent.set(`已经站了 ${duration.hours || 0}:${duration.minutes || 0}:${duration.seconds || 0}`)
 	}
 </script>
 
@@ -15,9 +28,12 @@
 	/>
 	<button
 		class="bg-gray-4 dark:bg-gray-500 hover:bg-gray-800 hover:dark:bg-gray-100 text-xl"
-		class:i-tabler-user-screen={$standing}
-		class:i-tabler-armchair2={!$standing}
-		on:click={() => stand_or_sit()}
+		class:i-tabler-user-screen={!$standing}
+		class:i-tabler-armchair2={$standing}
+		on:focus={showStandingStatus}
+		on:mouseenter={showStandingStatus}
+		on:mouseleave={() => tooltipContent.set('')}
+		on:click={stand_or_sit}
 	/>
 	<button
 		class="bg-gray-4 dark:bg-gray-500 hover:bg-gray-800 hover:dark:bg-gray-100 i-tabler-chevron-right text-xl"
